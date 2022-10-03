0997b9f41542c60ce4232bd81a08b0dfa3b96c31
Artur Szulist <arfes716@gmail.com>
2022-10-03 15:25:03 +0200

    first comming
    i dont wanna loose this no-progress-at-all files

---
+++ LAB01/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
+
+int main(){
+    using json = nlohmann::json;
+    using namespace std;
+    std::ifstream f("data\\data.json");
+    json data = json::parse(f);
+    //cout<<data<<endl;
+    return 0;
+}
