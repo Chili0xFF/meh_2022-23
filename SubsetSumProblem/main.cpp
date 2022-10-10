7b5fc227421da1b671ea8911448d6a2f29012dbf
Artur Szulist <arfes716@gmail.com>
2022-10-10 14:56:14 +0200

    3 first exes
    Class created, checksum created, show created

--- SubsetSumProblem/main.cpp
+++ SubsetSumProblem/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+class problem{
+    public:
+        vector<int> multiset;
+        int targetValue;
+        problem(int target,vector<int> set){
+            this->targetValue=target;
+            this->multiset=set;
+        };
+        void showMultiset(){
+            for(int i=0;i<this->multiset.size();i++){
+                cout<<this->multiset.at(i);
+            }
+        };
+        int checkIfCorrect(vector<int> subset){
+            int result=accumulate(subset.begin(),subset.end(),0);
+            return (targetValue-result);
+        };
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    problem problem(4,{1,2,3,4});
+    cout<<problem.checkIfCorrect({3,1})<<endl;
+    problem.showMultiset();
     return 0;
 }
