1da3b75d3f0f4e59f640ae252ac2c0c6093012bb
Artur Szulist <arfes716@gmail.com>
2022-10-24 15:18:08 +0200

    Added bruteforce with permutations. Full implementation

--- SubsetSumProblem/main.cpp
+++ SubsetSumProblem/main.cpp
@@ -2,31 +2,74 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <map>
 
 using namespace std;
 
-class problem{
+class Problem{
     public:
         vector<int> multiset;
+        vector<vector<int>> checked;
         int targetValue;
-        problem(int target,vector<int> set){
+        Problem(int target,vector<int> set){
             this->targetValue=target;
             this->multiset=set;
         };
         void showMultiset(){
-            for(int i=0;i<this->multiset.size();i++){
-                cout<<this->multiset.at(i);
+            cout<<"{"<<this->multiset.at(0);
+            for(int i=1;i<this->multiset.size();i++){
+                cout<<", "<<this->multiset.at(i);
             }
+            cout<<"}\n";
         };
-        int checkIfCorrect(vector<int> subset){
-            int result=accumulate(subset.begin(),subset.end(),0);
-            return (targetValue-result);
+        int checkHowCorrect(vector<int> subset){
+            int result=accumulate(subset.begin(),subset.end(),0);  //Funkcja kosztu
+            int differenceFromZero = result-targetValue;
+            return abs(differenceFromZero);
         };
 };
 
+
+
+vector<int> SolveBruteForce(Problem problem){
+    vector<int> multiset = problem.multiset;
+    vector<int> tempResult;
+    for(int i=1;i<=problem.multiset.size();i++)        //Ilość liczb w subsecie
+    {
+        //sort(multiset.begin(),multiset.end());
+        do{
+            tempResult.clear();
+            for(int j=0;j<i;j++){
+                tempResult.push_back(multiset.at(j));
+            }
+
+            if(count(problem.checked.begin(), problem.checked.end(), tempResult)==0){
+                cout<<"Subset: ";
+                cout<<"{"<<tempResult.at(0);
+                for(int j=1;j<tempResult.size();j++){
+                    cout<<", "<<tempResult.at(j);
+                }
+                cout<<"}\n";
+                int cost = problem.checkHowCorrect(tempResult);
+                //cout<<"Koszt: "<<cost<<endl;
+                if(cost==0){
+                    cout<<"FOUND CORRECT SUBSET!";
+                    return tempResult;
+                }
+                problem.checked.push_back(tempResult);
+            }
+
+
+        }while(next_permutation(multiset.begin(),multiset.end()));
+    }
+    return problem.multiset;
+}
+
 int main() {
-    problem problem(4,{1,2,3,4});
-    cout<<problem.checkIfCorrect({3,1})<<endl;
+    Problem problem(14,{1,2,3,3,4,5,6,7,8,1,2});
+    //cout<<"Odleglosc wyniku od celu: "<<problem.checkHowCorrect({3,1})<<endl;
     problem.showMultiset();
+    //vector<int> vector;
+    SolveBruteForce(problem);
     return 0;
 }
