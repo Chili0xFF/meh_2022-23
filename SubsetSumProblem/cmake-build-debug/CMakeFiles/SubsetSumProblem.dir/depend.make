828b84576bf59188d364b81ecedd0fd228e971fa
Chili0xFF <75846282+Chili0xFF@users.noreply.github.com>
2022-11-07 20:51:55 +0100

    AllAlgorithms

---
+++ SubsetSumProblem/cmake-build-debug/CMakeFiles/SubsetSumProblem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SubsetSumProblem.
+# This may be replaced when dependencies are built.
--- SubsetSumProblem/main.cpp
+++ SubsetSumProblem/main.cpp
@@ -47,16 +47,79 @@ class Problem{
 vector<int> SolveRandomTry(Problem problem, int iterations=1000);
 
 vector<int> SolveClimbing(Problem problem, int iterations=1000);
+vector<int> SolveRandomClimbing(Problem problem, int iterations=1000);
 
 vector<int> SolveBruteForce(Problem problem);
 
+vector<vector<int>> findNeighbour(const vector<int>& vector1);
+
+vector<int> translate(vector<int> vector1, vector<int> vector2);
+
+
+
 int main() {
-    Problem problem(60,{2,4,8,16,2,4,8,16,2,4,8,16});
+    Problem problem(75,{2,2,2,2,2,2,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16});
     //problem.showMultiset();
     //SolveBruteForce(problem);
-    SolveRandomTry(problem,10);
-    //SolveClimbing(problem);
+    //SolveRandomTry(problem,10);
+    SolveClimbing(problem);
+    SolveRandomClimbing(problem);
     return 0;
+}
+
+vector<int> SolveRandomClimbing(Problem problem, int iterations) {
+    vector<int> binaryZeroes = problem.binaryResult;
+    vector<int> bestSolution = problem.multiset;
+    //Generate size
+    int size = (mt1()%(problem.multiset.size()-1))+1;
+    //Generate numbers;
+    for(int j=0;j<size;j++) {
+        int indexToOne = mt1() % (problem.multiset.size());                           //generating a random subset
+        if (binaryZeroes.at(indexToOne) == 0)binaryZeroes.at(indexToOne) = 1;
+        else j--;
+    }
+
+    for(int i=0;i<iterations;i++){
+        vector<vector<int>> neighbours = findNeighbour(binaryZeroes);                       //Generating vector filled with new binary forms
+        vector<int> tempResult;
+        //BinaryZeros = Random binary subset
+        //best solution = max
+        //
+        tempResult = translate(problem.multiset,neighbours.at(0));
+        if(problem.checkHowCorrect(tempResult)==0){
+            cout<<"FOUND SUBSET!"<<endl;
+            return tempResult;
+        }
+        int randomIndex = mt1()%neighbours.size();
+        tempResult= translate(problem.multiset,neighbours.at(randomIndex));
+        if(problem.checkHowCorrect(tempResult)==0){
+            cout<<"FOUND SUBSET!"<<endl;
+            return tempResult;
+        }
+        if(problem.checkHowCorrect(bestSolution)>problem.checkHowCorrect(tempResult)){
+            bestSolution = tempResult;
+            binaryZeroes=neighbours.at(randomIndex);
+        }
+        cout<<"Subset: ";
+        cout<<"{"<<bestSolution.at(0);
+        for(int j=1;j<bestSolution.size();j++){
+            cout<<", "<<bestSolution.at(j);
+        }
+        cout<<"}";
+        int cost = problem.checkHowCorrect(bestSolution);
+        cout<<" HowFar: "<<cost<<"\n";
+
+    }
+    cout<<"No subset found"<<endl;
+    cout<<"Best solution: ";
+    cout<<"{"<<bestSolution.at(0);
+    for(int j=1;j<bestSolution.size();j++){
+        cout<<", "<<bestSolution.at(j);
+    }
+    cout<<"}";
+    int cost = problem.checkHowCorrect(bestSolution);
+    cout<<" HowFar: "<<cost<<"\n";
+    return bestSolution;
 }
 
 vector<int> SolveBruteForce(Problem problem){
@@ -108,10 +171,69 @@ vector<int> SolveBruteForce(Problem problem){
 vector<int> SolveClimbing(Problem problem, int iterations) {
     vector<int> binaryZeroes = problem.binaryResult;
     vector<int> bestSolution = problem.multiset;
-    for(int i=0;i<iterations;i++){
-        
+    //Generate size
+    int size = (mt1()%(problem.multiset.size()-1))+1;
+    //Generate numbers;
+    for(int j=0;j<size;j++) {
+        int indexToOne = mt1() % (problem.multiset.size());                           //generating a random subset
+        if (binaryZeroes.at(indexToOne) == 0)binaryZeroes.at(indexToOne) = 1;
+        else j--;
     }
-    return {0};
+
+    for(int i=0;i<iterations;i++){
+        vector<vector<int>> neighbours = findNeighbour(binaryZeroes);                       //Generating vector filled with new binary forms
+        vector<int> tempResult;
+        //BinaryZeros = Random binary subset
+        //best solution = max
+        //
+        tempResult = translate(problem.multiset,neighbours.at(0));
+        if(problem.checkHowCorrect(tempResult)==0){
+            cout<<"FOUND SUBSET!"<<endl;
+            return tempResult;
+        }
+        for(int j=1;j<neighbours.size();j++){
+            tempResult = translate(problem.multiset,neighbours.at(j));
+            if(problem.checkHowCorrect(tempResult)==0){
+                cout<<"FOUND SUBSET!"<<endl;
+                return tempResult;
+            }
+            if(problem.checkHowCorrect(bestSolution)>problem.checkHowCorrect(tempResult)){
+                bestSolution = tempResult;
+                binaryZeroes=neighbours.at(j);
+            }
+        }
+        cout<<"Subset: ";
+        cout<<"{"<<bestSolution.at(0);
+        for(int j=1;j<bestSolution.size();j++){
+            cout<<", "<<bestSolution.at(j);
+        }
+        cout<<"}";
+        int cost = problem.checkHowCorrect(bestSolution);
+        cout<<" HowFar: "<<cost<<"\n";
+
+    }
+    cout<<"No subset found"<<endl;
+    cout<<"Best solution: ";
+    cout<<"{"<<bestSolution.at(0);
+    for(int j=1;j<bestSolution.size();j++){
+        cout<<", "<<bestSolution.at(j);
+    }
+    cout<<"}";
+    int cost = problem.checkHowCorrect(bestSolution);
+    cout<<" HowFar: "<<cost<<"\n";
+    return bestSolution;
+}
+
+vector<vector<int>> findNeighbour(const vector<int>& binary) {
+    vector<vector<int>> neighbours;
+    for(int i=0;i<binary.size();i++){
+        vector<int> changedBinary = binary;
+        int changedIndex = mt1()%binary.size();
+        changedBinary.at(changedIndex)+=1;
+        changedBinary.at(changedIndex)%=2;
+        neighbours.push_back(changedBinary);
+    }
+    return neighbours;
 }
 
 vector<int> SolveRandomTry(Problem problem, int iterations) {
@@ -127,12 +249,8 @@ vector<int> SolveRandomTry(Problem problem, int iterations) {
             if(binaryZeroes.at(indexToOne)==0)binaryZeroes.at(indexToOne)=1;
             else j--;
         }
-        vector<int> tempResult;
-        for (int j = 0; j < binaryZeroes.size(); ++j) {
-            if(binaryZeroes.at(j)==1){
-                tempResult.push_back(problem.multiset.at(j));
-            }
-        }
+        vector<int> tempResult = translate(problem.multiset,binaryZeroes);
+
         cout<<"Subset: ";
         cout<<"{"<<tempResult.at(0);
         for(int j=1;j<tempResult.size();j++){
@@ -158,4 +276,14 @@ vector<int> SolveRandomTry(Problem problem, int iterations) {
     int cost = problem.checkHowCorrect(bestSolution);
     cout<<" HowFar: "<<cost<<"\n";
     return bestSolution;
+}
+
+vector<int> translate(vector<int> multiset, vector<int> binaryZeroes) {
+    vector<int> Result;
+    for (int j = 0; j < binaryZeroes.size(); ++j) {
+        if(binaryZeroes.at(j)==1){
+            Result.push_back(multiset.at(j));
+        }
+    }
+    return Result;
 }
